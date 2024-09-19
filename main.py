08e363b3b92f63221a2bf13b8a523552a0521ce7
Jeffrey Murati <jeffreymurati@Xhefris-Air.fritz.box>
2024-09-20 00:22:56 +0200

    last changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 
 def load_data(file_path):
     """Loads a JSON file"""
-    with open(file_path, "r") as handle:
+    with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 
@@ -31,7 +31,7 @@ def iterating_data(animals_data):
 
 def load_template():
     """for loading data from html file"""
-    with open('animals_template.html', 'r') as file:
+    with open('animals_template.html', 'r', encoding="utf-8") as file:
         html_template = file.read()
         return html_template
 
@@ -41,11 +41,26 @@ def serialize_animal(animal_obj):
     output = '<li class="cards__item">\n'
     output += f"<div class='card__title'>Name: {animal_obj['name']}</div><br/>\n"
     output += f"Diet: {animal_obj['characteristics']['diet']}<br/>\n"
-    output += f"First location: {animal_obj['locations'][0]}<br/>\n"
+    output += f"First Location: {animal_obj['locations'][0]}<br/>\n"
+    habitat = animal_obj['characteristics'].get('habitat')
+    if habitat and len(habitat) > 0:
+        output += f"Habitat: {habitat[:]}<br/>\n"  # Using the first habitat if there are multiple
+
+    lifespan = animal_obj['characteristics'].get('lifespan')
+    if lifespan:
+        output += f"Lifespan: {lifespan[:]}<br/>\n"
+
+    common_name = animal_obj.get('common_name')
+    if common_name:
+        output += f"Common Name: {common_name[:]}<br/>\n"
 
     animal_type = animal_obj['characteristics'].get('type')
     if animal_type:
         output += f"Type: {animal_type}<br/>\n"
+
+    color = animal_obj['characteristics'].get('color')
+    if color:
+        output += f"Color: {color[:]}<br/>\n"
 
     output += '</li>\n'
     return output
@@ -62,13 +77,12 @@ def replace_animals_info(html_data, string_data_animals):
     replaced_data = html_data.replace('__REPLACE_ANIMALS_INFO__', string_data_animals)
     print(replaced_data)
 
-    with open('edited_animals.html', 'w') as file:
+    with open('edited_animals.html', 'w', encoding="utf-8") as file:
         html_template = file.write(replaced_data)
 
 
 def main():
     animals_data = load_data('animals_data.json')
-    iteration_data = iterating_data(animals_data)
     html_data = load_template()
     string_data_animals = string_creation_data(animals_data)
     replace_animals_info(html_data, string_data_animals)
