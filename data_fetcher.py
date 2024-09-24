b3373cdad0236c0497b835eac60ba321da3cc688
Jeffrey Murati <jeffreymurati@Xhefris-Air.fritz.box>
2024-09-24 22:32:53 +0200

    adding api

---
+++ data_fetcher.py
@@ -0,0 +1,18 @@
+import requests
+
+def fetch_data(animal_name):
+    api_url = f'https://api.api-ninjas.com/v1/animals?name={animal_name}'
+    api_key = "mJhYs7Z+4LdRKUkKW4TT7A==z8iKL7GxYnmLK8yb"
+
+    try:
+        response = requests.get(api_url, headers={'X-Api-Key': api_key})
+
+        if response.status_code == requests.codes.ok:
+            data = response.json()
+            return data
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
